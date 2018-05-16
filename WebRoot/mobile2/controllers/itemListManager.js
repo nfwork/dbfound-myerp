a6c5b37fb527a8b47b3dd93a232413f2e898f7f0
huangjiong <huangjiong@gomo.com>
2018-05-16 16:23:46 +0800

    修复异步bug

--- WebRoot/mobile2/controllers/itemListManager.js
+++ WebRoot/mobile2/controllers/itemListManager.js
@@ -17,50 +17,54 @@ angular.module('myerpApp').config(function($routeProvider) {
 				period_name : "---请选择---"
 			})
 			$scope.preiods = res.datas;
+			$scope.preiod = res.datas[res.datas.length - 1].period_id;
+			initTable();
 		}
 	})
 
 	$scope.item_id = 0;
 
-	$('#table').bootstrapTable({
-		url : "../exp/item.query",
-		striped : true,
-		pagination : true,
-		contentType : "application/x-www-form-urlencoded",
-		method : 'post',
-		pageSize : 5,
-		responseHandler : tableResponseHandle,
-		queryParams : function(params) {
-			params.period_id = $("#preiod").val();
-			params.description = $("#description").val();
-			params.start = params.offset;
-			return params;
-		},
-		onClickCell : function(field, value, row, element) {
-			if (field == "item_num") {
-				return;
+	function initTable() {
+		$('#table').bootstrapTable({
+			url : "../exp/item.query",
+			striped : true,
+			pagination : true,
+			contentType : "application/x-www-form-urlencoded",
+			method : 'post',
+			pageSize : 5,
+			responseHandler : tableResponseHandle,
+			queryParams : function(params) {
+				params.period_id = $scope.preiod;
+				params.description = $scope.description;
+				params.start = params.offset;
+				return params;
+			},
+			onClickCell : function(field, value, row, element) {
+				if (field == "item_num") {
+					return;
+				}
+				$scope.item_id = row.item_id;
+				$('#detailTable').bootstrapTable('refresh');
+			},
+			onLoadSuccess : function() {
+				var a = $(".pull-left");
+				// a.hide();
 			}
-			$scope.item_id = row.item_id;
-			$('#detailTable').bootstrapTable('refresh');
-		},
-		onLoadSuccess : function() {
-			var a = $(".pull-left");
-			// a.hide();
-		}
-	});
+		});
 
-	$('#detailTable').bootstrapTable({
-		url : "../exp/itemLine.query",
-		striped : true,
-		contentType : "application/x-www-form-urlencoded",
-		method : 'post',
-		responseHandler : tableResponseHandle,
-		queryParams : function(params) {
-			params.period_id = $("#preiod").val();
-			params.item_id = $scope.item_id;
-			return params;
-		},
-	});
+		$('#detailTable').bootstrapTable({
+			url : "../exp/itemLine.query",
+			striped : true,
+			contentType : "application/x-www-form-urlencoded",
+			method : 'post',
+			responseHandler : tableResponseHandle,
+			queryParams : function(params) {
+				params.period_id = $scope.period;
+				params.item_id = $scope.item_id;
+				return params;
+			},
+		});
+	}
 
 	$scope.refresh = function() {
 		$scope.item_id = 0;
