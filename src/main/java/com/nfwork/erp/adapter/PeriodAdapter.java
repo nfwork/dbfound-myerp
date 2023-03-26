40ef86291ef6832abe84b4bcdae228b3bb5df639
huangjiong <huangjiong@gomo.com>
2023-03-26 19:07:38 +0800

    添加自动生成代码

---
+++ src/main/java/com/nfwork/erp/adapter/PeriodAdapter.java
@@ -0,0 +1,26 @@
+package com.nfwork.erp.adapter;
+
+import com.nfwork.dbfound.core.Context;
+import com.nfwork.dbfound.model.ModelEngine;
+import com.nfwork.dbfound.model.adapter.QueryAdapter;
+import com.nfwork.dbfound.model.bean.Param;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+
+public class PeriodAdapter implements QueryAdapter<Object> {
+
+    @Override
+    public void beforeQuery(Context context, Map<String, Param> params) {
+       int size = ModelEngine.query(context,"report/homeAnalysis","getCurrentPeriod").getDatas().size();
+       if( size == 0){
+           Date date = new Date();
+           SimpleDateFormat format = new SimpleDateFormat("yyyy-MM");
+           SimpleDateFormat formatS = new SimpleDateFormat("yyyy年MM月");
+           context.setParamData("period_code",format.format(date));
+           context.setParamData("period_code",formatS.format(date));
+           ModelEngine.execute(context,"report/homeAnalysis","autoCreate");
+       }
+    }
+}
