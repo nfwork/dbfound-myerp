dc421ba9c7748ccdea4813c1eae601cfb77c1b31
huangjiong <huangjiong@gomo.com>
2023-04-07 23:26:02 +0800

    优化逻辑

--- weerp/app.js
+++ weerp/app.js
@@ -1,30 +1,10 @@
 // app.js
 App({
   onLaunch() {
-    wx.login({
-      success: res => {
-        wx.request({
-          url: 'https://dbfound.3g.net.cn/dbfound/sys/wxLogin.execute!login',
-          header:{ "Cookie":wx.getStorageSync('cookies')},
-          method:"POST",
-          data:{
-            js_code: res.code 
-          },
-          success : (res)=> {
-            if(res.data.success){
-              let cookieString = "";
-              if(res.cookies.length >0){
-                for(const index in res.cookies){
-                  cookieString = cookieString + res.cookies[index] +";"
-                }
-              }
-              wx.setStorageSync('cookies', cookieString);
-            }
-          }
-        })
-      }
-    })
+    //let result = await this.login();
+    //console.log(result)
   },
+  
   globalData: {
     userInfo: null
   },
--- weerp/pages/home/home.js
+++ weerp/pages/home/home.js
@@ -7,7 +7,8 @@ Page({
   data: {
       user_name:"",
       totalexp:0,
-      accounts_exp:[]
+      accounts_exp:[],
+      inited:false
   },
 
   menuTap(e){
@@ -34,11 +35,83 @@ Page({
     }
   },
 
+  async init(){
+    wx.showLoading({
+      title: '正在加载中',
+    })
+    let code = await this.getJsCode();
+    let result = await this.wxLogin(code);
+    result = await this.getHomeAnalysis();
+    this.setData({inited:true});
+    wx.hideLoading();
+  },
+
+  getJsCode(){
+    return new Promise((resolve,reject)=>{
+      wx.login({
+        success: res => {
+          resolve(res.code)
+        }
+      })
+    });
+  },
+
+  wxLogin(jsCode){
+    return new Promise((resolve,reject)=>{
+      wx.request({
+        url: 'https://dbfound.3g.net.cn/dbfound/sys/wxLogin.execute!login',
+        header:{ "Cookie":wx.getStorageSync('cookies')},
+        method:"POST",
+        data:{
+          js_code: jsCode 
+        },
+        success : (res)=> {
+          if(res.data.success){
+            let cookieString = "";
+            if(res.cookies.length >0){
+              for(const index in res.cookies){
+                cookieString = cookieString + res.cookies[index] +";"
+              }
+            }
+            wx.setStorageSync('cookies', cookieString);
+          }
+        },
+        complete : () =>{
+          resolve(1);
+        }
+      })
+    });
+  },
+
+  getHomeAnalysis(){
+    return new Promise((resolve,reject)=>{
+      wx.request({
+        url: 'https://dbfound.3g.net.cn/dbfound/report/homeAnalysis.query',
+        header:{ "Cookie":wx.getStorageSync('cookies')},
+        success : (res)=> {
+          if(res.data.success){
+            let totalAccount = res.data.datas.shift();
+            this.setData({
+              user_name:res.data.outParam.user_name,
+              accounts_exp:res.data.datas,
+              totalexp:totalAccount.totalexp
+            });
+          }else if(res.data.timeout){
+            wx.navigateTo({url: "../login/login"});
+          }
+        },
+        complete : () =>{
+          resolve(1);
+        }
+      })
+    });
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    
+    this.init();
   },
 
   /**
@@ -52,26 +125,9 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow() {
-    wx.request({
-      url: 'https://dbfound.3g.net.cn/dbfound/report/homeAnalysis.query',
-      header:{ "Cookie":wx.getStorageSync('cookies')},
-      success : (res)=> {
-        if(res.data.success){
-          let totalAccount = res.data.datas.shift();
-          this.setData({
-            user_name:res.data.outParam.user_name,
-            accounts_exp:res.data.datas,
-            totalexp:totalAccount.totalexp
-          });
-        }else if(res.data.timeout){
-          wx.navigateTo({url: "../login/login"});
-        }
-      }
-    })
-  },
-
-  getPhoneNumber (e) {
-    console.log(e)
+    if(this.data.inited){
+      this.getHomeAnalysis();
+    }
   },
 
   /**
