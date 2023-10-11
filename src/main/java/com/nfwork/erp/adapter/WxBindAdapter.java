6ac25b936b235bba34791358d651be3d3705f330
huangjiong <huangjiong@gomo.com>
2023-10-11 16:36:36 +0800

    升级dbfound3.4版本

--- src/main/java/com/nfwork/erp/adapter/WxBindAdapter.java
+++ src/main/java/com/nfwork/erp/adapter/WxBindAdapter.java
@@ -6,11 +6,14 @@ import com.nfwork.dbfound.model.adapter.ExecuteAdapter;
 import com.nfwork.dbfound.model.bean.Param;
 import com.nfwork.dbfound.util.JsonUtil;
 import com.nfwork.dbfound.util.LogUtil;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ClassicHttpResponse;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.util.Timeout;
 
 import java.io.IOException;
 import java.util.Map;
@@ -35,12 +38,20 @@ public class WxBindAdapter implements ExecuteAdapter {
                 wxs = ModelEngine.query(context,"sys/wxLogin",null).getString("wxs");
             }
             HttpGet httpGet = new HttpGet(String.format(wxUrl, wxs, jsCode));
-            try(CloseableHttpClient httpClient = HttpClients.createDefault();
-                CloseableHttpResponse response = httpClient.execute(httpGet)){
+            try(CloseableHttpClient httpClient = HttpClients.createDefault()){
+
+                RequestConfig config = RequestConfig.custom().setConnectTimeout(Timeout.ofSeconds(10))
+                        .setConnectionRequestTimeout(Timeout.ofSeconds(10))
+                        .setResponseTimeout(Timeout.ofSeconds(10))
+                        .build();
+                httpGet.setConfig(config);
+
+                ClassicHttpResponse response = httpClient.execute(httpGet);
                 String res = EntityUtils.toString(response.getEntity());
+                response.close();
                 return JsonUtil.jsonToMap(res).get("openid").toString();
             }
-        } catch (IOException e) {
+        } catch (IOException | ParseException e) {
             LogUtil.error(e.getMessage(),e);
             throw new RuntimeException("获取openid失败");
         }
