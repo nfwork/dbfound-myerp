12357740332e48d78e62ee7e5fc173bf4a93a041
huangjiong <huangjiong@gomo.com>
2024-05-22 11:52:12 +0800

    优化

--- weacc/vue.config.js
+++ weacc/vue.config.js
@@ -4,24 +4,6 @@ module.exports = defineConfig({
   publicPath: "",
   devServer: {
     host: "0.0.0.0",
-    port: 8081,
-    open: false,
-    proxy: {
-      "/proxy": {
-        target: `https://dbfound.3g.net.cn/dbfound/`,
-        changeOrigin: true
-      }
-    }
-  },
-  pwa: {
-    name: "weacc",
-    themeColor: "#428bca",
-    msTileColor: "#efefef",
-    appleMobileWebAppCapable: "yes",
-    appleMobileWebAppStatusBarStyle: "black",
-    workboxPluginMode: "InjectManifest",
-    workboxOptions: {
-      swSrc: "./src/service-worker.js"
-    }
+    port: 8081
   }
 })
