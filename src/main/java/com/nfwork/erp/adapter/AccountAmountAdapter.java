73717723da5f300a716b09d266aed046b2f1a31e
huangjiong <huangjiong@gomo.com>
2026-05-08 16:31:02 +0800

    优化

--- src/main/java/com/nfwork/erp/adapter/AccountAmountAdapter.java
+++ src/main/java/com/nfwork/erp/adapter/AccountAmountAdapter.java
@@ -6,7 +6,6 @@ import com.nfwork.dbfound.model.ModelEngine;
 import com.nfwork.dbfound.model.adapter.QueryAdapter;
 import com.nfwork.dbfound.model.bean.Param;
 import com.nfwork.erp.dto.AccountAmountItem;
-import com.nfwork.erp.dto.AccountGroupItem;
 
 import java.util.List;
 import java.util.Map;
@@ -14,21 +13,16 @@ import java.util.Map;
 public class AccountAmountAdapter implements QueryAdapter<AccountAmountItem> {
     @Override
     public void afterQuery(Context context, Map<String, Param> params, QueryResponseObject<AccountAmountItem> responseObject) {
-        int periodForm = params.get("period_code_from").getIntValue();
-        int periodTo = params.get("period_code_to").getIntValue();
         Map<String,AccountAmountItem> accountMap = responseObject.getMap("account_id");
 
-        List<AccountGroupItem> itemList = ModelEngine.query(context,"report/accountAmountQuery","getGroupData",AccountGroupItem.class).getDatas();
-        for (AccountGroupItem item : itemList){
+        List<AccountAmountItem> itemList = ModelEngine.query(context,"report/accountAmountQuery","getGroupData",AccountAmountItem.class).getDatas();
+        for (AccountAmountItem item : itemList){
             AccountAmountItem amountItem = accountMap.get(item.getAccountId().toString());
-            if(item.getPeriodCode() >= periodForm && item.getPeriodCode() <= periodTo){
-                amountItem.setEmergeAmount(amountItem.getEmergeAmount().add(item.getAmount()));
-            }else{
-                amountItem.setRemaindAmount(amountItem.getRemaindAmount().add(item.getAmount()));
+            if(amountItem != null){
+                amountItem.setEmergeAmount(item.getEmergeAmount());
+                amountItem.setRemaindAmount(item.getRemaindAmount());
+                amountItem.setEndAmount(amountItem.getEmergeAmount().add(amountItem.getRemaindAmount()));
             }
-        }
-        for(AccountAmountItem item : responseObject.getDatas()){
-            item.setEndAmount(item.getEmergeAmount().add(item.getRemaindAmount()));
         }
     }
 }
