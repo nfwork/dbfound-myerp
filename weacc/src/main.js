cdc01fe475e6a273ed22d7307f56c5af0721c870
huangjiong <huangjiong@gomo.com>
2026-05-09 16:07:49 +0800

    添加pwa缓存处理

--- weacc/src/main.js
+++ weacc/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import router from './router';
+import './registerServiceWorker';
 import MySelect from '@/components/MySelect.vue';
 import MyCalendar from '@/components/MyCalendar.vue';
 import 'vant/lib/index.css';
--- weacc/vue.config.js
+++ weacc/vue.config.js
@@ -5,5 +5,49 @@ module.exports = defineConfig({
   devServer: {
     host: "0.0.0.0",
     port: 8081
+  },
+  pwa: {
+    name: "WE记账",
+    themeColor: "#428bca",
+    msTileColor: "#428bca",
+    manifestPath: "manifest.json",
+    workboxPluginMode: "GenerateSW",
+    workboxOptions: {
+      skipWaiting: true,
+      clientsClaim: true,
+      cleanupOutdatedCaches: true,
+      navigateFallback: "index.html",
+      exclude: [/\.map$/, /robots\.txt$/],
+      runtimeCaching: [
+        {
+          urlPattern: /\/images\/.*\.(?:png|jpg|jpeg|gif|svg|webp|ico)$/i,
+          handler: "CacheFirst",
+          options: {
+            cacheName: "weacc-static-images",
+            cacheableResponse: {
+              statuses: [0, 200]
+            },
+            expiration: {
+              maxEntries: 80,
+              maxAgeSeconds: 30 * 24 * 60 * 60
+            }
+          }
+        },
+        {
+          urlPattern: ({ request }) => ["script", "style", "font"].includes(request.destination),
+          handler: "StaleWhileRevalidate",
+          options: {
+            cacheName: "weacc-static-assets",
+            cacheableResponse: {
+              statuses: [0, 200]
+            },
+            expiration: {
+              maxEntries: 60,
+              maxAgeSeconds: 7 * 24 * 60 * 60
+            }
+          }
+        }
+      ]
+    }
   }
 })
